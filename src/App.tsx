6b3c2486be7d36ae6f19c27c66db3e244df3dd28
nandemo3 <mytittyinjp@gmail.com>
2022-01-21 15:22:05 +0900

    fitst commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import DeckGL from "@deck.gl/react";
 import { StaticMap } from "react-map-gl";
-
-import { CoordinateType } from "./types";
-import { getRouteLayer } from "./layers/RouteLayer";
-
-type DirectionType = "from" | "to";
-type CoordinateStateType = CoordinateType | null;
 
 const MAPBOX_ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
@@ -22,33 +16,12 @@ const INITIAL_VIEW_STATE = {
 // DeckGL react component
 function App() {
   const [layers, setLayers] = useState<any>(null);
-  const [direction, setDirection] = useState<DirectionType>("from");
-  const [fromCoordinate, setFromCoordinate] =
-    useState<CoordinateStateType>(null);
-  const [toCoordinate, setToCoordinate] = useState<CoordinateStateType>(null);
-
-  useEffect(() => {
-    if (fromCoordinate !== null && toCoordinate !== null) {
-      getRouteLayer(fromCoordinate, toCoordinate).then((res: any) => {
-        setLayers(res);
-      });
-    }
-  }, [fromCoordinate, toCoordinate]);
 
   return (
     <DeckGL
       initialViewState={INITIAL_VIEW_STATE}
       controller={true}
       layers={layers}
-      onClick={(info: any) => {
-        if (direction === "from") {
-          setFromCoordinate(info.coordinate);
-          setDirection("to");
-        } else {
-          setToCoordinate(info.coordinate);
-          setDirection("from");
-        }
-      }}
     >
       <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
     </DeckGL>
